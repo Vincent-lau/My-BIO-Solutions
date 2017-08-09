54dddc77de236c36c4cedb77dcb2c79b34f78094
Vincent-lau <guang243683484@163.com>
2017-08-09 11:10:19 +0800

    Create BIO 2016 Q3.cpp

---
+++ Searching/BIO 2016 Q3.cpp
@@ -0,0 +1,80 @@
+//problem source:BIO 2016 Question3: Prime Connections
+//main process:Bfs+linear prime number filtration
+//Real test case didn't have upper limit greater than 1e6,so Bfs will be enough. But if the range is greater,dBfs might be used
+#include <iostream>
+#include <cstdio>
+#include<cstring>
+#include <algorithm>
+#include<set>
+#include<cmath>
+#include<vector>
+#include<ctime>
+#include<queue>
+using namespace std;
+struct Status{
+	int num;
+	int steps;
+	Status(int n,int s):num(n),steps(s){}
+	Status(){}
+};
+int l;
+bool isPrime[20000000];
+bool used[20000000];
+vector<int> primes;
+void getPrime(int N){
+	memset(isPrime,true,sizeof(isPrime));
+	isPrime[1]=0;
+	primes.clear();
+	for(int i=2;i<=N;++i){
+		if(isPrime[i])
+			primes.push_back(i);
+		for(unsigned int j=0;j<primes.size();++j){
+			if(i*primes[j]<=N)
+				isPrime[i*primes[j]]=0;
+			else
+				break;
+			if(i%primes[j]==0)
+				break;
+		}
+	}
+}
+int Bfs(int startNum,int endNum){
+	queue<Status> q;
+	memset(used,0,sizeof(used));
+	q.push(Status(startNum,1));
+	used[startNum]=1;
+	while(!q.empty()){
+		Status s=q.front();
+		q.pop();
+		if(s.num==endNum)
+			return s.steps;
+		int L=0;
+		while(true){
+			int Inc=s.num+(1<<L);
+			if(Inc <=l && isPrime[Inc] && !used[Inc]){
+				q.push(Status(Inc,s.steps+1));
+				used[Inc]=1;
+			}
+			int Dec=s.num-(1<<L);
+			if(Dec>1 && isPrime[Dec] && !used[Dec]){
+				q.push(Status(Dec,s.steps+1));
+				used[Inc]=1;
+			}
+			if(Inc>l && Dec<1)
+				break;
+			++L;
+		}
+	}
+	return -1;
+}
+int main(){
+	freopen("data.in","r",stdin);
+//	freopen("data.out","w",stdout);
+   	getPrime(1<<24);
+   	int s,e;
+   	while(cin>>l>>s>>e)
+   		cout<<Bfs(s,e)<<endl;
+    fclose(stdin); //fclose(stdout);
+  	return 0;
+    
+}
